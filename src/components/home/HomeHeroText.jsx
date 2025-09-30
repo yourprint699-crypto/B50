e6733187a284275d5cfe286199f916cf5599e7a3
videochamar-sketch <videochamar@gmail.com>
2025-09-30 21:46:52 +0530

    Updated HomeHeroText.jsx

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -8,7 +8,7 @@ const HomeHeroText = () => {
           We keep the
         </div>
         <div className="text-[12vw] sm:text-[9vw] lg:text-[9.5vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[8vw] text-layer-3 flex-wrap justify-center mb-2 sm:mb-0">
-          <span>the edits</span>
+          <span>edits</span>
           <div className="h-[8vw] w-[20vw] sm:h-[7vw] sm:w-[16vw] rounded-full overflow-hidden mx-2 sm:mx-2 glass glow-accent flex-shrink-0 my-1 sm:my-0">
             <video
               className="h-full w-full object-cover hero-inline-video"
