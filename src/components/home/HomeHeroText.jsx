c74267c412f88c5ddcabbfeff03bd7a47ccbfdcf
videochamar-sketch <videochamar@gmail.com>
2025-10-02 20:34:59 +0530

    Updated index.css

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomeHeroText = () => {
   return (
-    <div className="font-[font1] text-center relative depth-4 px-4 flex-1 flex items-center justify-center">
+    <div className="text-center relative depth-4 px-4 flex-1 flex items-center justify-center" style={{ fontFamily: '"Canela", serif' }}>
       <div className="w-full">
         <div className="text-[12vw] sm:text-[9vw] lg:text-[9.5vw] justify-center flex items-center uppercase leading-[10vw] sm:leading-[7.5vw] lg:leading-[8vw] text-layer-3 mb-2 sm:mb-0">
           We keep the
